df1cd7bdcdc58b08f098f2e6aa951a2905d367aa
Vaishnavi Singh Rajput <vaishnavirajput981@gmail.com>
2024-01-14 23:54:33 +0530

    updated files

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const addTask = document.getElementById('add-task')
 const addButton = document.getElementById('add-button')
 const taskList = document.getElementById('task-list')
 
-addButton.addEventListener('click', function(event){
+
+function submit(event){
     if(addTask.value === ""){
         alert("You must write something!")
     }
@@ -10,5 +11,45 @@ addButton.addEventListener('click', function(event){
         let li = document.createElement("li");
         li.innerHTML = addTask.value;
         taskList.appendChild(li)
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
-})
+    
+    addTask.value = "";
+    saveData();
+}
+
+addButton.addEventListener('click',submit)
+
+
+taskList.addEventListener("click", function(event){
+    if(event.target.tagName === "LI"){
+        event.target.classList.toggle("checked")
+        saveData();
+    }
+    else if(event.target.tagName === "SPAN"){
+        event.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+addTask.addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();
+        submit();
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem("data", taskList.innerHTML);
+}
+function savedData(){
+    taskList.innerHTML = localStorage.getItem("data");
+}
+savedData();
+
+
+
+
+
